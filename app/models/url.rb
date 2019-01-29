3e49e6965bd9b07b312aaeab741fc56df9676843
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-29 15:46:51 +0530

    instance var gone - model-url

--- app/models/url.rb
+++ app/models/url.rb
@@ -55,15 +55,16 @@ class Url < ApplicationRecord
   def self.shorten_url(params)
     #Rails.cache.clear
     begin
-      @entry = Url.create!(params)
-      @prefix= Rails.cache.fetch(params[:domain], :expires_in => 5.minutes) do 
+      entry = Url.create!(params)
+      prefix= Rails.cache.fetch(params[:domain], :expires_in => 5.minutes) do 
         DomainPrefixHelper.find_domain_prefix(params[:domain])
       end	        
-      @short_url=UrlsHelper.md5hash(params[:long_url])
-      @short_url=UrlsHelper.check_collision_md5(@short_url)
-      params[:short_url]=@short_url
-      @entry.update_attributes(params)
-      return {"Status"=>"Success","short_url"=> @prefix+@short_url,"long_url"=>params[:long_url],"domain"=>params[:domain]}
+      short_url=UrlsHelper.md5hash(params[:long_url])
+      short_url=UrlsHelper.check_collision_md5(short_url)
+      params[:short_url]=short_url
+      entry.update_attributes(params)
+      return {"Status"=>"Success","short_url"=> prefix+short_url,"long_url"=>params[:long_url],"domain"=>params[:domain]}
+
 
     rescue Exception => exception
 
@@ -72,18 +73,18 @@ class Url < ApplicationRecord
       elsif exception.to_s.include? "blank"
         return {"Status" => "Error","Error"=>"Enter All Params"}
       else
-        @row  =	Rails.cache.fetch(params[:long_url], :expires_in => 5.minutes) do 
+        row  =	Rails.cache.fetch(params[:long_url], :expires_in => 5.minutes) do 
           Url.where(long_url: params[:long_url]).first
         end
-        @prefix= Rails.cache.fetch(params[:domain], :expires_in => 5.minutes) do 
-          DomainPrefixHelper.find_domain_prefix(@row[:domain])
+        prefix= Rails.cache.fetch(params[:domain], :expires_in => 5.minutes) do 
+          DomainPrefixHelper.find_domain_prefix(row[:domain])
         end
 
-        if @row[:short_url] == nil
-          @row.destroy
+        if row[:short_url] == nil
+          row.destroy
           return {"Status" => "Error","Error"=>"Something Went Wrong"}
         else
-          return {"Status"=>"Already Exists","short_url"=>@prefix+@row[:short_url],"long_url"=>@row[:long_url],"domain"=>@row[:domain]}
+          return {"Status"=>"Already Exists","short_url"=>prefix+row[:short_url],"long_url"=>row[:long_url],"domain"=>row[:domain]}
         end
       end
     end
@@ -100,13 +101,13 @@ class Url < ApplicationRecord
     end
 
     begin
-      @row=	Rails.cache.fetch(params[:short_url], :expires_in => 5.minutes) do
+      row=	Rails.cache.fetch(params[:short_url], :expires_in => 5.minutes) do
         Url.where(short_url: params[:short_url]).first
       end
-      @prefix= Rails.cache.fetch(@row[:domain], :expires_in => 5.minutes) do 
-        DomainPrefixHelper.find_domain_prefix(@row[:domain])
+      prefix= Rails.cache.fetch(row[:domain], :expires_in => 5.minutes) do 
+        DomainPrefixHelper.find_domain_prefix(row[:domain])
       end
-      return {"Status"=>"OK !","long_url"=>@row[:long_url],"domain"=>@row[:domain],"short_url"=>@prefix+params[:short_url]}
+      return {"Status"=>"OK !","long_url"=>row[:long_url],"domain"=>row[:domain],"short_url"=>prefix+params[:short_url]}
     rescue Exception => exception
       return {"Status"=>"Nothing Found !"}
     end	
@@ -114,21 +115,21 @@ class Url < ApplicationRecord
 
 
   def self.custom_search(params)
-  @field = params[:mode]+".trigram"
-  @query = params[:q] 
-  @urls = self.__elasticsearch__.search(
+  field = params[:mode]+".trigram"
+  query = params[:q] 
+  urls = self.__elasticsearch__.search(
   {
    query: {
     bool: {
      must: [{
       term: {
-       "#{@field}":"#{@query}"
+       "#{field}":"#{query}"
       }
      }]
     }
    }
   }).records
-  return @urls
+  return urls
   end
 
   def put_in_report_table_async
