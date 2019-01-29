dfefaa00a3dc2c70b97fd995142e3b09ca8dc905
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-29 15:48:56 +0530

    instance var gone

--- app/workers/url_worker.rb
+++ app/workers/url_worker.rb
@@ -1,13 +1,13 @@
 class UrlWorker
   include Sidekiq::Worker
   def perform(*args)
-    @date = Date.today.to_s
-    if Report.where(date:@date).first
-      @report = Report.where(date:@date).first
-      @report.no_of_urls = @report.no_of_urls + 1
-      @report.save
+    date = Date.today.to_s
+    if Report.where(date:date).first
+      report = Report.where(date:date).first
+      report.no_of_urls = report.no_of_urls + 1
+      report.save
     else
-      Report.create(:date => @date, :no_of_urls =>1)
+      Report.create(:date => date, :no_of_urls =>1)
     end
   end	
 end
