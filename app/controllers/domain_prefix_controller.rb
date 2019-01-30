a5e3ea84dddcc914f5a9d75d66994ff3e8e6926a
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-30 12:29:03 +0530

    domain prefix creation form done

--- app/controllers/domain_prefix_controller.rb
+++ app/controllers/domain_prefix_controller.rb
@@ -1,4 +1,20 @@
 class DomainPrefixController < ApplicationController
-  def index
+  def domain_form
+  end
+
+  def create_domain
+  	params[:prefix]=params[:prefix] + '/'
+  	if DomainPrefix.create_entry(domain_params)
+  		flash[:status] = "Created !!"
+      	redirect_to home_page_index_path
+  	else
+  		flash[:Error] = "Taken Try Again Either Domain or Prefix is taken"
+      	redirect_to domain_prefix_domain_form_path
+  	end
+  end
+
+  private
+  def domain_params
+    params.permit(:domain,:prefix)
   end
 end
--- app/models/domain_prefix.rb
+++ app/models/domain_prefix.rb
@@ -1,8 +1,18 @@
 class DomainPrefix < ApplicationRecord
   validates :domain , presence: true,uniqueness: true
   validates :prefix,presence: true, uniqueness: true
+
   def self.find_prefix(domain)
     return DomainPrefix.where(domain: domain).first
+  end
+
+  def self.create_entry(params)
+  	begin
+  		DomainPrefix.create!(params)
+  		return true
+  	rescue Exception => exception
+  		return false
+  	end
   end
 
 end
