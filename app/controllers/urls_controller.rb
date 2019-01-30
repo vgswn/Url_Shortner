cd8b5cd79debb16bbb84e5e453c34fb533f2c0c1
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-30 15:54:57 +0530

    multiple returns baaki hai

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -12,61 +12,13 @@ class UrlsController < ApplicationController
   end
 
   def api_post_shorten_url
-    if check_params(url_params).class == Hash.new.class
-      render json: check_params(url_params),status: :bad_request
-      return
-    end
-    params[:long_url] = params[:long_url].strip
-    params[:domain]=Domainatrix.parse(params[:long_url]).domain
-    domain_row = check_domain_valid_url(params[:domain])
-    if domain_row.class == Hash.new.class
-      render json: domain_row,status: :bad_request
-      return
-    end
-    prefix = domain_row[:prefix]
-    url = Url.new(url_params)
-    url.save
-    errors = url.find_errors
-    response_of_validity = check_valid_url(errors,url_params,prefix)
-    if response_of_validity.class == Hash.new.class
-      render json: response_of_validity,status:response_of_validity[:status]
-      return
-    end
-    short_url=UrlsHelper.md5hash(params[:long_url])
-    short_url=UrlsHelper.check_collision_md5(short_url)
-    params[:short_url]=short_url
-    url.update_attributes(url_params)
-    render json: {
-      status: :accepted,
-      short_url:prefix+short_url,
-      long_url:params[:long_url],
-      domain:params[:domain]
-    },status: :accepted
+    response = shorten_url(url_params)
+    render json: response,status: response[:status]
   end
 
   def api_get_short_url
-    if check_params(url_params).class == Hash.new.class
-      render json: check_params(url_params),status: :bad_request
-      return
-    end
-    params[:short_url] = params[:short_url].strip
-    if params[:short_url].include?("/") == true
-      params[:short_url]=params[:short_url].split('/').last
-    end
-    url= Url.where(url_params).first
-    if url.nil?
-      render json: {
-        status: :not_found
-        },status: :not_found
-    else
-      prefix = DomainPrefix.where(domain: url[:domain]).first[:prefix]
-      render json: {
-        status: :ok,
-        long_url:url[:long_url],
-        domain:url[:domain],
-        short_url:prefix+url[:short_url]
-      },status: :ok
-    end
+    response = short_url(url_params)
+    render json: response,status: response[:status]
   end
 
   def convert_long_url_to_short_url
@@ -75,10 +27,7 @@ class UrlsController < ApplicationController
       redirect_to urls_long_url_to_short_url_path
     else
       params[:long_url] = params[:long_url].strip
-      params[:long_url]=NormalizeUrl.process(params[:long_url])
-      params[:domain]=Domainatrix.parse(params[:long_url]).domain
-      puts params[:domain]
-      @result = Url.shorten_url(url_params)
+      @result = shorten_url(url_params)
       render '/urls/show'
     end
   end
@@ -89,10 +38,7 @@ class UrlsController < ApplicationController
       redirect_to urls_short_url_to_long_url_path
     else
       params[:short_url] = params[:short_url].strip
-      if params[:short_url].include?("/") == true
-        params[:short_url]=params[:short_url].split('/').last
-      end
-      @result = Url.short_url(url_params)
+      @result = short_url(url_params)
       render '/urls/show'
     end
   end
@@ -103,7 +49,57 @@ private
   end
 
   def shorten_url(params)
+    if check_params(params).class == Hash.new.class
+      return check_params(url_params)
+    end
+    params[:long_url] = params[:long_url].strip
+    params[:domain]=Domainatrix.parse(params[:long_url]).domain
+    domain_row = check_domain_valid_url(params[:domain])
+    if domain_row.class == Hash.new.class
+      return domain_row
+    end
+    prefix = domain_row[:prefix]
+    url = Url.new(params)
+    url.save
+    errors = url.find_errors
+    response_of_validity = check_valid_url(errors,params,prefix)
+    if response_of_validity.class == Hash.new.class
+      return response_of_validity
+    end
+    short_url=UrlsHelper.md5hash(params[:long_url])
+    short_url=UrlsHelper.check_collision_md5(short_url)
+    params[:short_url]=short_url
+    url.update_attributes(params)
+    return {
+      status: :accepted,
+      short_url:prefix+short_url,
+      long_url:params[:long_url],
+      domain:params[:domain]
+    }
+  end
 
+  def short_url(params)
+    if check_params(params).class == Hash.new.class
+      return check_params(params)
+    end
+    params[:short_url] = params[:short_url].strip
+    if params[:short_url].include?("/") == true
+      params[:short_url]=params[:short_url].split('/').last
+    end
+    url= Url.where(params).first
+    if url.nil?
+      return {
+        status: :not_found
+        }
+    else
+      prefix = DomainPrefix.where(domain: url[:domain]).first[:prefix]
+      return {
+        status: :ok,
+        long_url:url[:long_url],
+        domain:url[:domain],
+        short_url:prefix+url[:short_url]
+      }
+    end
   end
 
   def check_params(params)
@@ -153,6 +149,5 @@ private
       return true
     end
   end
-
 end
 
