dc864f1989365e0889828b04c5d1430ecf39a0e2
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-30 10:16:41 +0530

    validations done

--- app/models/domain_prefix.rb
+++ app/models/domain_prefix.rb
@@ -1,5 +1,6 @@
 class DomainPrefix < ApplicationRecord
-
+  validates :domain , presence: true,uniqueness: true
+  validates :prefix,presence: true, uniqueness: true
   def self.find_prefix(domain)
     return DomainPrefix.where(domain: domain).first
   end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,7 @@
 class Report < ApplicationRecord
   validates :date ,uniqueness: true
+  validates :no_of_urls , presence: true
+  
   def self.get_report
     report = Report.all.order(:date)
   end
