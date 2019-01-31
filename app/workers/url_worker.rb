e694346988fa526ffa2f532743ef2c936afa61a9
Vipul kumar <iit2015038@iiita.ac.in>
2019-01-31 15:36:56 +0530

    Documentation

--- app/workers/url_worker.rb
+++ app/workers/url_worker.rb
@@ -1,5 +1,11 @@
 class UrlWorker
   include Sidekiq::Worker
+=begin
+  **Author:** Vipul Kumar     
+  **Common Name:** Worker that increase count of no_of_urls in the Report database asynchronously   
+  **End points:** Other services       
+  **Host:** localhost:3000    
+=end
   def perform(*args)
     date = Date.today.to_s
     if Report.where(date:date).first
